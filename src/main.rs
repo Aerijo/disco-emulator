23560dbe4c124ec449780c8fd11074d3c94d1bb4
Benjamin Gray <bengray178@gmail.com>
2019-06-02 00:46:28 +1000

    start on input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#[macro_use] extern crate lazy_static;
 extern crate regex;
 
 use std::num::Wrapping;
+use std::option::Option;
 use std::io::{self, Write};
 use regex::Regex;
 
@@ -53,6 +55,21 @@ impl CPU {
         self.registers[15] += Wrapping(1);
     }
 
+    fn set_flags_nzcv (&mut self, v1: Wrapping<u32>, v2: Wrapping<u32>, result: Wrapping<u32>) {
+        self.flags.n = (result.0 as i32) < 0;
+        self.flags.z = result.0 == 0;
+        self.flags.c = result < v1 || result < v2;
+        self.flags.v = (((v1.0 & v2.0 & !result.0) | (!v1.0 & !v2.0 & result.0)) & (1 << 31)) > 0;
+    }
+
+    fn mov_imm (&mut self, dest: usize, val: u32, flags: bool) {
+        if flags {
+            self.flags.n = (val as i32) < 0;
+            self.flags.z = val == 0;
+        }
+        self.set_reg(dest, val);
+    }
+
     fn mov_reg (&mut self, to: usize, from: usize) {
         assert!(to <= 15 && from <= 15);
         self.inc_pc();
@@ -72,12 +89,7 @@ impl CPU {
         let v1 = self.registers[first];
         let v2 = self.registers[second];
         let result = v1 + v2;
-
-        self.flags.n = (result.0 as i32) < 0;
-        self.flags.z = result.0 == 0;
-        self.flags.c = result < v1 || result < v2;
-        self.flags.v = ((!(v1.0 ^ v2.0) ^ result.0) & (1 << 31)) == 0; // highest bit was same,
-
+        self.set_flags_nzcv(v1, v2, result);
 
         self.registers[dest] = result;
     }
@@ -90,45 +102,59 @@ impl CPU {
 }
 
 
+enum Instruction {
+    MovImm { to: usize, val: u32, flags: bool },
+    MovReg { to: usize, from: usize, flags: bool },
+    AddImm { dest: usize, first: usize, val: u32, flags: bool },
+    AddReg { dest: usize, first: usize, second: usize, flags: bool },
+    SubImm { dest: usize, first: usize, val: u32, flags: bool },
+    SubReg { dest: usize, first: usize, second: usize, flags: bool },
+}
+
+
+fn parse_input (input: String) -> Option<Instruction> {
+    lazy_static! {
+        static ref instr_regex: Regex = Regex::new(r"^\s*([a-zA-Z0-9]+)").unwrap();
+    }
+
+    if !instr_regex.is_match(&input) { return None };
+
+    let instr = instr_regex.captures(&input).unwrap().get(1).unwrap().as_str();
+
+    match instr {
+        "mov" => return Some(Instruction::MovImm { to: 0, val: (-100i32 as u32), flags: false }),
+        "movs" => return Some(Instruction::MovImm { to: 0, val: (-100i32 as u32), flags: true }),
+        _ => return None,
+    }
+}
+
+
 fn main () {
     println!("Welcome to ARM simulator");
 
     let mut regs = CPU::new();
 
-    let instruction_regex = Regex::new(r"(add|sub|mov) r([0-9]+), r([0-9]+)(?:, r([0-9]+))?").unwrap();
-
     loop {
-        println!("{:?}", regs);
-        print!("> ");
+        print!("\n{:?}\n> ", regs);
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
 
         io::stdin().read_line(&mut input).unwrap();
 
-        match instruction_regex.captures(&input) {
-            Some(caps) => {
-                let dest: usize = caps.get(2).unwrap().as_str().parse().unwrap();
-                let first: usize = caps.get(3).unwrap().as_str().parse().unwrap();
+        let instruction = parse_input(input);
 
-                match caps.get(1).unwrap().as_str() {
-                    "add" => {
-                        let second: usize = caps.get(4).unwrap().as_str().parse().unwrap();
-                        regs.adds_reg(dest, first, second);
+        match instruction {
+            Some (instr) => {
+                match instr {
+                    Instruction::MovImm { to, val, flags } => {
+                        println!("Valid mov");
+                        regs.mov_imm(to, val, flags);
                     },
-                    "sub" => {
-                        let second: usize = caps.get(4).unwrap().as_str().parse().unwrap();
-                        regs.sub_reg(dest, first, second);
-                    },
-                    "mov" => {
-                        regs.mov_reg(dest, first);
-                    }
-                    _ => {}
+                    _ => {println!("Valid input");}
                 }
             },
-            None => {
-                println!("Invalid input");
-            }
+            None => println!("Invalid input"),
         }
     }
 }
